3eb851785f41821a5e186867780ee45ed99cf5ba
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-03-19 19:40:34 +0400

    Добавлен класс пути, соединяющего поставщика и потребителя.

---
+++ TransportationProblem/Domain/Route.cs
@@ -0,0 +1,28 @@
+﻿namespace TransportationProblem.Domain
+{
+    /// <summary>
+    /// Путь, соединяющий поставщика и потребителя.
+    /// </summary>
+    public class Route
+    {
+        /// <summary>
+        /// Поставщик из которого поставляется продукция.
+        /// </summary>
+        public Supplier Supplier { get; set; }
+
+        /// <summary>
+        /// Потребитель, в которого поставляется продукция.
+        /// </summary>
+        public Consumer Consumer { get; set; }
+
+        /// <summary>
+        /// Стоимость передачи единицы продукции по данному пути.
+        /// </summary>
+        public double Cost { get; set; }
+
+        /// <summary>
+        /// Объем поставки продукции по данному пути.
+        /// </summary>
+        public double Volume { get; set; }
+    }
+}
