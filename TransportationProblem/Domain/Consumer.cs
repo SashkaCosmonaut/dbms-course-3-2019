91f430901c518c4c091eb19f5c4b374282f05267
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-03-19 19:33:14 +0400

    Добавлен класс потребителя.

---
+++ TransportationProblem/Domain/Consumer.cs
@@ -0,0 +1,18 @@
+﻿namespace TransportationProblem.Domain
+{
+    /// <summary>
+    /// Класс потребителя.
+    /// </summary>
+    public class Consumer
+    {
+        /// <summary>
+        /// Наименование потребителя.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Тебуемый объем поставок для потребителя.
+        /// </summary>
+        public double Demand { get; set; }
+    }
+}
