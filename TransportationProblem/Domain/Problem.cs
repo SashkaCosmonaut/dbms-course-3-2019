92777a74070850526704f59fd82e4430b1db42f9
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-03-19 19:47:35 +0400

    Добавлен класс задачи, решение которой необходимо найти.

---
+++ TransportationProblem/Domain/Problem.cs
@@ -0,0 +1,19 @@
+﻿namespace TransportationProblem.Domain
+{
+    /// <summary>
+    /// Задача, решение которой необходимо найти.
+    /// </summary>
+    public class Problem
+    {
+        /// <summary>
+        /// Наименование задачи.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Множество путей, соединяющих определённых поставщиков и потребителей 
+        /// в данной задаче, образующих матрицу путей.
+        /// </summary>
+        public Route[] Routes { get; set; }
+    }
+}
