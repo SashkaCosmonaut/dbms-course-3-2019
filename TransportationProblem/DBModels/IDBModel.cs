f702b5411761bbd8adc6105c716ffe8bf14c08d6
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-03-19 20:43:26 +0400

    Добавлен интерфейс, задающий поведение классам моделей БД.

---
+++ TransportationProblem/DBModels/IDBModel.cs
@@ -0,0 +1,28 @@
+﻿using TransportationProblem.Domain;
+
+namespace TransportationProblem.DBModels
+{
+    /// <summary>
+    /// Интерфейс, задающий поведение классам моделей БД.
+    /// </summary>
+    public interface IDBModel
+    {
+        /// <summary>
+        /// Получить все имеющиеся в БД задачи.
+        /// </summary>
+        /// <returns>Массив имеющихся задач.</returns>
+        Problem[] GetProblems();
+
+        /// <summary>
+        /// Добавить новую задачу в БД.
+        /// </summary>
+        /// <param name="problem">Объект задачи для добавления.</param>
+        void AddProblem(Problem problem);
+
+        /// <summary>
+        /// Удалить существующую задачу из БД.
+        /// </summary>
+        /// <param name="problem">Объект удаляемой задачи.</param>
+        void DeleteProblem(Problem problem);
+    }
+}
