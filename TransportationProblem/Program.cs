bb98ec22037fac6c4ec496713da3f4683ba1c660
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-03-29 19:58:03 +0400

    Добавлены try-catch.

--- TransportationProblem/Program.cs
+++ TransportationProblem/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using TransportationProblem.DBModels;
 using TransportationProblem.DBModels.JSON;
 
@@ -26,14 +25,21 @@ namespace TransportationProblem
         /// <param name="args">Аргументы командной строки.</param>
         public static void Main(string[] args)
         {
-            var model = ChooseDB();
-
-            if (model == null) return;
-
-            if (!model.Init(AskForDBFile()))
+            try
             {
-                Console.WriteLine("Произошла ошибка инициализации БД!");
-                return;
+                var model = ChooseDB();
+
+                if (model == null) return;
+
+                if (!model.Init(AskForDBFile()))
+                {
+                    Console.WriteLine("Произошла ошибка инициализации БД!");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка работы программы! " + ex.Message);
             }
         }
 
@@ -43,17 +49,25 @@ namespace TransportationProblem
         /// <returns>Модель выбранной БД.</returns>
         public static IDBModel ChooseDB()
         {
-            Console.WriteLine("Выберете БД, введите её название из списка имеющихся:");
+            try
+            {
+                Console.WriteLine("Выберете БД, введите её название из списка имеющихся:");
 
-            foreach (var dbName in DBModels.Keys)
-                Console.WriteLine(dbName);
+                foreach (var dbName in DBModels.Keys)
+                    Console.WriteLine(dbName);
 
-            var choice = Console.ReadLine();
+                var choice = Console.ReadLine();
 
-            if (DBModels.ContainsKey(choice))   // Проверяем наличие БД с таким названием
-                return DBModels[choice];
+                if (DBModels.ContainsKey(choice))   // Проверяем наличие БД с таким названием
+                    return DBModels[choice];
 
-            Console.WriteLine("Некорректное название БД!");
+                Console.WriteLine("Некорректное название БД!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
             return null;
         }
 
@@ -63,9 +77,24 @@ namespace TransportationProblem
         /// <returns>Строка с путём до файла с БД.</returns>
         public static string AskForDBFile()
         {
-            Console.WriteLine("Введите путь до файла с БД или папки:");
+            var path = "";
 
-            return Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Введите путь до файла с БД или папки:");
+
+                do
+                {
+                    path = Console.ReadLine();
+
+                } while (string.IsNullOrWhiteSpace(path));  // Ждём пока не введут не пустую строку
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return path;
         }
     }
 }
